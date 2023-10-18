7648e1f1736528f481d61d41cf06622faadd561a
Evan Jones <ej@evanjones.ca>
2023-10-18 11:19:40 -0400

    rustsetenvcrash: Add getenv version (does not crash)

--- rustsetenvcrash/src/main.rs
+++ rustsetenvcrash/src/main.rs
@@ -1,15 +1,43 @@
 use std::{error::Error, net::ToSocketAddrs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("spawning thread to lookup localhost ...");
-    let t = std::thread::spawn(lookup_localhost);
+    // terrible argument parsing without dependencies
+    const TRY_GETENV: &str = "try_getenv";
+    let program_args: Vec<String> = std::env::args().collect();
+    if program_args.len() > 2 {
+        eprintln!("ERROR: rustsetenvcrash only accepts one optional argument: {TRY_GETENV}; found {} args",
+            program_args.len());
+        std::process::exit(1);
+    }
+    if program_args.len() == 2 && program_args[1] != TRY_GETENV {
+        eprintln!(
+            "ERROR: rustsetenvcrash only accepts one optional argument: {TRY_GETENV}; found {}",
+            program_args[1]
+        );
+        std::process::exit(1);
+    }
+    let try_getenv = program_args.len() == 2;
 
     const NUM_ITERATIONS: usize = 100;
+    println!("will call std::env::set_var() {NUM_ITERATIONS} times ...");
+
+    let t = if try_getenv {
+        println!(
+            "spawning thread to call std::env::var (will not crash: Rust holds lock for getenv)..."
+        );
+        std::thread::spawn(do_getenv)
+    } else {
+        println!("spawning thread to lookup localhost (may crash with glibc; run with {TRY_GETENV} to call getenv instead)...");
+        std::thread::spawn(lookup_localhost)
+    };
+
     for i in 0..NUM_ITERATIONS {
         std::env::set_var(format!("ENV_VAR_{i}"), "value");
     }
 
     t.join().expect("BUG thread must succeed");
+
+    println!("exiting without error");
     Ok(())
 }
 
@@ -17,5 +45,14 @@ fn lookup_localhost() {
     let addrs_iter = "localhost:1".to_socket_addrs().unwrap();
     for addr in addrs_iter {
         println!("localhost: ip={} port={}", addr.ip(), addr.port());
+    }
+}
+
+fn do_getenv() {
+    for _ in 0..1000 {
+        let r = std::env::var("doesnotexist");
+        if let Ok(_) = r {
+            panic!("env var should not exist");
+        }
     }
 }
